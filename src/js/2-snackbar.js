0c8e5076a2b4245625e892afb6d085b1cbc48080
Maksum9 <krukm187@gmail.com>
2023-12-23 13:28:18 +0200

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector("[name='delay']");
+  const stateInput = document.querySelector("[name='state']:checked");
+
+  const delay = parseInt(delayInput.value, 10);
+  const state = stateInput.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === "fulfilled") {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise.then(
+    (value) => {
+      iziToast.success({
+        title: "Fulfilled",
+        message: `✅ Fulfilled promise in ${value}ms`,
+        position: "topRight"
+      });
+    },
+    (reason) => {
+      iziToast.error({
+        title: "Rejected",
+        message: `❌ Rejected promise in ${reason}ms`,
+        position: "topRight"
+      });
+    }
+  );
+});
